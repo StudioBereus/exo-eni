a0ccdb54dcc23a9f5cad2c64bffc240ae8ad6302
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2023-02-10 11:09:45 +0100

    Add files via upload

---
+++ exo-eni-1-bis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exo_eni_1_bis
+{
+    class Program
+    {
+        //Afficher les nombres impairs entre 1 et 10
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                if (i % 2 == 1) 
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
---
+++ exo-eni-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exo_eni_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculer la somme de tous les chiffres précédent un chiffre donné
+            Console.WriteLine("Veuillez indiquer un nombre entier");
+            if(int.TryParse(Console.ReadLine(), out int number))
+            {
+                int sum = 0;
+                for (int i = 0; i < number + 1; i++) 
+                {
+                    sum += i;
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
