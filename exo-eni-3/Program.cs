76900e6c695b827813b4e617ab1392d67ea9be64
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2023-02-13 14:17:06 +0100

    Add files via upload

---
+++ exo-eni-3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exo_eni_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Avec une structure tant que, faire un algorithme qui effectue une multiplication par addition successives 
+            Console.WriteLine("Veuillez indiquer deux chiffres");
+            string[] rep = Console.ReadLine().Split(' ');
+            if(int.TryParse(rep[0], out int a) && int.TryParse(rep[1], out int b)){
+                int res = 0;
+                while (b != 0)
+                {
+                    res += a;
+                    b--;
+                }
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
