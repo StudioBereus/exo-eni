09072cd9caea36f1206aa1d9863bea0fbd8387d7
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2023-02-13 16:01:18 +0100

    Add files via upload

---
+++ exo-eni-4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exo_eni_4
+{
+    class Program
+    {
+        //Un film a coûté 150 millions d’euros. La première semaine, le film réalise une recette de 31 millions d’euros.
+        //Chaque semaine qui passe, la recette est 20 % inférieure à celle de la semaine précédente.
+        //Donnez l’algorithme qui permet d’indiquer au bout de combien de semaines le film est bénéficiaire.
+
+        static void Main(string[] args)
+        {
+            double investissement = 150;
+            double recette = 31;
+            int nbSemaines = 0;
+            while (investissement >= 0) 
+            {
+                nbSemaines++;
+                investissement -= recette; 
+                recette *= 0.8;
+            }
+            Console.WriteLine("Nombre de semaines avant que le film ne soit rentable : " + nbSemaines);
+        }
+    }
+}
