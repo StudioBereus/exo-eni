71c2470701606ad56bc9d244f519b7218d2f87da
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2023-02-14 09:41:12 +0100

    Add files via upload

---
+++ exo-eni-5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exo_eni_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Écrire un algorithme qui convertit un nombre entier décimal en binaire
+            string binary = "";
+            Console.WriteLine("Veuillez entrer un nombre entier");
+            if (int.TryParse(Console.ReadLine(), out int number)){
+                while (number != 0)
+                {
+                    if (number % 2 == 0)
+                    {
+                        binary = 0 + "" + binary;
+                    }
+                    else
+                    {
+                        binary = 1 + "" + binary;
+                    }
+                    number /= 2;
+                }
+            }
+            Console.WriteLine(binary);
+        }
+    }
+}
